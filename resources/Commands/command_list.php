814c8e225dcfdf812e2e5eb2dc205ae792496b55
sinisa <miskovic.sinisa44@gmail.com>
2020-02-12 15:34:01 +0100

    command list update

--- resources/Commands/command_list.php
+++ resources/Commands/command_list.php
@@ -5,32 +5,37 @@ return [
         [
             'name'        => 'make:migration',
             'description' => 'creates new table',
-            'command'      => 'php application make:migration TableName_table'
+            'command'      => 'php cmd make:migration TableName_table'
         ],
         [
             'name'        => 'migrate',
             'description' => 'migrate table to database',
-            'command'     => 'php application migrate'
+            'command'     => 'php cmd migrate'
         ],
         [
             'name'        => 'drop:table',
             'description' => 'drops table from database',
-            'command'     => 'php application drop:table TableName_table'
+            'command'     => 'php cmd drop:table TableName_table'
         ],
         [
             'name'        => 'show:commands',
             'description' => 'show all commands',
-            'command'     => 'php application show:commands'
+            'command'     => 'php cmd show:commands'
         ],
         [
             'name'        => 'show:columns',
             'description' => 'show column type for specific table',
-            'command'     => 'php application show:columns TableName'
+            'command'     => 'php cmd show:columns TableName'
         ],
         [
             'name'        => 'show:tables',
             'description' => 'list all tables from current database',
-            'command'     => 'php application show:tables'
+            'command'     => 'php cmd show:tables'
+        ],
+        [
+            'name'        => 'rename:table',
+            'description' => 'renames table in current database',
+            'command'     => 'php cmd rename:table'
         ],
       
     ],
